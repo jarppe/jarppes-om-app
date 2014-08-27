f5c5d933d8787f2b52b91d2f32ed3d552b22d9f7
jarppe <jarppe@metosin.fi>
2014-08-27 22:20:01 +0300

    cleanup

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jarppes-om-app/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple lein template to create Clojure, ClojureScript, OM application"
+  :url "https://github.com/jarppe/jarppes-om-app"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
